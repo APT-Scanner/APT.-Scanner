4bcab356279f018cee6c27057be12e082b1ff221
Adi Karif <adika3@mta.ac.il>
2025-04-05 15:59:07 +0300

    installed front end packages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function App() {
+  const name = 'APT.Scanner';
+  return (
+    <div>
+      {' '}
+      <h1> שלום {name}</h1> <p>אפליקציית המלצות לדירות</p>{' '}
+    </div>
+  );
+}
